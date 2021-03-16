600b33cd6a571f00f042e52ebbd036894a1f779b
윤승혁 <dbsrbtkd94@gmail.com>
2021-03-16 19:15:49 +0900

    Update : google 계정 로그인 기능 추가

--- src/main/java/com/yshyuk/springboot/web/IndexController.java
+++ src/main/java/com/yshyuk/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.yshyuk.springboot.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.yshyuk.springboot.config.auth.dto.SessionUser;
 import com.yshyuk.springboot.service.PostsService;
 import com.yshyuk.springboot.web.DTO.PostsResponseDto;
 
@@ -15,10 +18,15 @@ import lombok.RequiredArgsConstructor;
 public class IndexController {
 
 	private final PostsService postsService;
+	private final HttpSession httpSession;
 	
 	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("posts",postsService.findAllDesc());
+		SessionUser user = (SessionUser) httpSession.getAttribute("user");
+		if(user!=null) {
+			model.addAttribute("username", user.getName());
+		}
 		return "index";
 	}
 	
